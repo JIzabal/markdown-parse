3f358cfdfd1542eb2508b1a74b67e965af9ea31e
JIzabal <jizabal@ucsd.edu>
2022-02-20 17:43:09 -0800

    Added test case for test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         MarkdownParse.getLinks("");
         assertEquals(2, 1 + 1); 
+    }
+
+    @Test
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 }
